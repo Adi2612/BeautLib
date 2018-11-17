c6801617cd7793ca7c9e79085bb485ebb332d6a7
Aaditya Arora <b17071@students.iitmandi.ac.in>
2018-11-17 22:33:25 +0530

    as

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-!=m84p@c&h%y17@7)_mua$2hp(&$x@@ecu9@o1@a=hp2qh)@v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1' , 'localhost' ,'*']
 
 
 # Application definition
@@ -82,14 +82,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'osho',
+#         'USER': 'creativecodes',
+#         'PASSWORD': 'zulip',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'osho',
-        'USER': 'creativecodes',
-        'PASSWORD': 'zulip',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
